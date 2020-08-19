984f18e11cfe00bbaffb4f03b374ae54e5f892fe
John Burkey <johnburkey@brighten.ai>
2020-08-19 16:18:05 -0500

    soname

--- Package.swift
+++ Package.swift
@@ -16,12 +16,24 @@ let package = Package(
   ],
   targets: [
     .target(name: "CryptoSwift",
+            exclude: [
+                "CryptoSwift/Info.plist"
+            ],
             linkerSettings: [
                             .unsafeFlags([ "-Xlinker","-soname=libCryptoSwift.so"],.when(platforms: [.android])),
                              ]
 ),
-    .testTarget(name: "CryptoSwiftTests", dependencies: ["CryptoSwift"]),
-    .testTarget(name: "TestsPerformance", dependencies: ["CryptoSwift"])
+//    .testTarget(name: "CryptoSwiftTests", dependencies: ["CryptoSwift"]),
+//    .testTarget(name: "TestsPerformance", dependencies: ["CryptoSwift"])
   ],
   swiftLanguageVersions: [.v5]
 )
+//
+///Users/johnburkey/Projects/Brighten2020/CryptoSwift/Sources/CryptoSwift/Info.plist
+//
+//error: found 2 file(s) which are unhandled; explicitly declare them as resources or exclude from the target
+// Tests/CryptoSwiftTests/Info.plist
+// Tests/CryptoSwiftTests/Default-568h@2x.png
+//
+//error: found 1 file(s) which are unhandled; explicitly declare them as resources or exclude from the target
+// Tests/TestsPerformance/Info.plist
